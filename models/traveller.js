ca535e2b4e7913827391e6d8e187c929e26042aa
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-22 18:01:13 +0100

    get total journey distance

--- models/traveller.js
+++ models/traveller.js
@@ -3,15 +3,15 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-  return this.journeys.map((journey) => journey.startLocation);
+  return this.journeys.map(journey => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-  return this.journeys.map((journey) => journey.endLocation);
+  return this.journeys.map(journey => journey.endLocation);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-  return this.journeys.filter((journey) => journey.transport === transport);
+  return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
@@ -19,7 +19,7 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((totalDistance, journey) => totalDistance + journey.distance, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
